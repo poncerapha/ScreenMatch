ee9c8589b131dfad3b26854ecde5bdac40034fe4
Raphael Ponce Da Cruz Machado Fleury <raphael.fleury@americanas.io>
2023-10-02 10:17:40 -0300

    feat: create package and getters/setters

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
+import br.com.alura.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
-        myMovie.name = "O Poderoso Chefao";
-        myMovie.releaseDate = 1970;
-        myMovie.movieDuration = 180;
+        myMovie.setName("O Poderoso Chefao");
+        myMovie.setReleaseDate(1970);
+        myMovie.setMovieDuration(180);
+        myMovie.setIncludeInAccount(true);
 
         myMovie.renderMovieInfo();
         myMovie.sendReview(8);
         myMovie.sendReview(5);
         myMovie.sendReview(10);
 
-        System.out.println(myMovie.sumReviews);
-        System.out.println(myMovie.totalReviews);
-        System.out.println(myMovie.ratingAverage());
+        System.out.println("MEdia de avalicaoes do filme: " + myMovie.getRatingAverage());
     }
 }
